3e14928a389b9de76da75185295e78476b244ece
John Samuelson <johnsamuelson@me.com>
2014-05-29 19:12:32 -0600

    set main class manifest to point NumericValueConverter

---
+++ src/main/java/plantsmiles/NumericValueConverter.java
@@ -0,0 +1,10 @@
+package plantsmiles;
+
+/**
+ * Created by john.samuelson on 5/29/14.
+ */
+public class NumericValueConverter {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
